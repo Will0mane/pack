f6ae96020eed6843623a448a313d39fc51548bb8
Will0mane <100951314+Will0mane@users.noreply.github.com>
2026-03-13 17:43:30 +0100

    Preserve original cause chain in SystemException

--- api/src/main/java/me/will0mane/software/pack/api/exceptions/SystemException.java
+++ api/src/main/java/me/will0mane/software/pack/api/exceptions/SystemException.java
@@ -3,6 +3,6 @@ package me.will0mane.software.pack.api.exceptions;
 public class SystemException extends ConnectionException{
 
     public SystemException(Exception e) {
-        super(e.getMessage());
+        super(e);
     }
 }
